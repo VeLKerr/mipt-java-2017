3b1f219ec68278abcaacac4e8bd2cb147d28e893
Denis Kalinochkin <kalinochkind@gmail.com>
2017-09-22 09:37:25 +0300

    Task1: prototype

---
+++ homework-g695-kalinochkin/src/main/java/ru/mipt/java2017/homework/g695/kalinochkin/task1/App.java
@@ -0,0 +1,26 @@
+package ru.mipt.java2017.homework.g695.kalinochkin.task1;
+
+import ru.mipt.java2017.homework.base.task1.ParsingException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        MyCalculator calc = new MyCalculator();
+        String expression;
+        if (args.length >= 1) {
+            expression = args[0];
+        } else {
+            System.out.print("Enter an expression: ");
+            Scanner scanner = new Scanner(System.in);
+            expression = scanner.nextLine();
+        }
+        try {
+            double result = calc.calculate(expression);
+            System.out.print("Result: ");
+            System.out.println(result);
+        } catch (ParsingException e) {
+            System.out.println("Invalid expression");
+        }
+    }
+}
---
+++ homework-g695-kalinochkin/src/main/java/ru/mipt/java2017/homework/g695/kalinochkin/task1/MyCalculator.java
@@ -0,0 +1,11 @@
+package ru.mipt.java2017.homework.g695.kalinochkin.task1;
+
+import ru.mipt.java2017.homework.base.task1.ParsingException;
+import ru.mipt.java2017.homework.base.task1.Calculator;
+
+public class MyCalculator implements Calculator {
+
+    public double calculate(String expression) throws ParsingException {
+        return 0;
+    }
+}
