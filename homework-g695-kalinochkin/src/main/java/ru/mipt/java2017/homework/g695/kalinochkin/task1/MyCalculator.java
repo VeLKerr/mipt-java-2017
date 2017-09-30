cc51239c86ae61bb50dabdc9a905bc3326589710
Denis Kalinochkin <kalinochkind@gmail.com>
2017-09-30 23:41:06 +0300

    Bugfixes and docs

--- homework-g695-kalinochkin/src/main/java/ru/mipt/java2017/homework/g695/kalinochkin/task1/MyCalculator.java
+++ homework-g695-kalinochkin/src/main/java/ru/mipt/java2017/homework/g695/kalinochkin/task1/MyCalculator.java
@@ -8,10 +8,18 @@ import java.util.Stack;
 
 class MyCalculator implements Calculator {
 
+    /**
+     * Evaluates an expression.
+     *
+     * @param expression the expression to evaluate
+     * @return the value of the expression
+     * @throws ParsingException if the expression is invalid
+     */
     public double calculate(String expression) throws ParsingException {
         if (expression == null || expression.length() == 0) {
             throw new ParsingException("Empty expression");
         }
+        // tokens in postfix notation
         ArrayList<Token> tokens = reorder(tokenize(expression));
         Stack<Double> st = new Stack<>();
         for (Token t : tokens) {
@@ -41,9 +49,15 @@ class MyCalculator implements Calculator {
         return st.pop();
     }
 
-    protected int getPriority(Token t) {
+    /**
+     * Returns the precedence of the operation. Higher value means higher priority.
+     *
+     * @param t token with the operation
+     * @return precedence of the operation
+     */
+    protected int getPrecedence(Token t) {
         if (t.getType() == TokenType.UNARY_OP) {
-            return 100;
+            return 3;
         }
         if (t.getValue().equals("+") || t.getValue().equals("-")) {
             return 1;
@@ -51,10 +65,23 @@ class MyCalculator implements Calculator {
         return 2;
     }
 
+    /**
+     * Checks whether the operation is right-asociative.
+     *
+     * @param t token with the operation
+     * @return true if the operation is right-associative, false otherwise
+     */
     protected boolean isRightAssoc(Token t) {
         return t.getType() == TokenType.UNARY_OP;
     }
 
+    /**
+     * Evaluates the unary operation.
+     *
+     * @param op operation to evaluate
+     * @param a  argument of the operation
+     * @return the result of evaluation
+     */
     protected double evalUnary(String op, double a) {
         if (op.equals("+")) {
             return a;
@@ -65,6 +92,14 @@ class MyCalculator implements Calculator {
         return 0;
     }
 
+    /**
+     * Evaluates the binary operation.
+     *
+     * @param op operation to evaluate
+     * @param a  first argumrnt of the operation
+     * @param b  second argument of the operation
+     * @return the result of evaluation
+     */
     protected double evalBinary(String op, double a, double b) {
         if (op.equals("+")) {
             return a + b;
@@ -81,11 +116,18 @@ class MyCalculator implements Calculator {
         return 0;
     }
 
+    /**
+     * Converts the expression into a list of tokens.
+     *
+     * @param expression the expression to convert
+     * @return list of tokens
+     * @throws ParsingException if the expression could not be tokenized
+     */
     protected ArrayList<Token> tokenize(String expression) throws ParsingException {
         ArrayList<Token> tokens = new ArrayList<>();
-        String number = "";
-        TokenType last = TokenType.LEFTPAR;
-        int pars = 0;
+        String number = ""; // accumulates current numeric literal
+        TokenType last = TokenType.LEFTPAR; // the previous token
+        int pars = 0; // number of unclosed parentheses
         for (char c : (expression + ' ').toCharArray()) {
             if (Character.isDigit(c) || c == '.') {
                 if (last == TokenType.NUMBER) {
@@ -98,12 +140,14 @@ class MyCalculator implements Calculator {
                     last = TokenType.NUMBER;
                     number = "";
                 }
-                if (last == TokenType.LEFTPAR || last == TokenType.UNARY_OP || last == TokenType.BINARY_OP) {
+                if (last == TokenType.LEFTPAR || last == TokenType.BINARY_OP) {
                     if (c != '+' && c != '-') {
                         throw new ParsingException("Invalid unary operation: " + c);
                     }
                     tokens.add(new Token(TokenType.UNARY_OP, "" + c));
                     last = TokenType.UNARY_OP;
+                } else if (last == TokenType.UNARY_OP) {
+                    throw new ParsingException("Multiple unary operations are not allowed");
                 } else {
                     tokens.add(new Token(TokenType.BINARY_OP, "" + c));
                     last = TokenType.BINARY_OP;
@@ -146,6 +190,12 @@ class MyCalculator implements Calculator {
         return tokens;
     }
 
+    /**
+     * Converts the list of tokens in natural order into postfix notation.
+     *
+     * @param tokens the list of tokens
+     * @return the list of tokens in postfix notation
+     */
     protected ArrayList<Token> reorder(ArrayList<Token> tokens) {
         ArrayList<Token> result = new ArrayList<>();
         Stack<Token> stack = new Stack<>();
@@ -157,9 +207,10 @@ class MyCalculator implements Calculator {
                 case UNARY_OP:
                 case BINARY_OP:
                     while (!stack.empty() && stack.peek().getType() != TokenType.LEFTPAR &&
-                            getPriority(stack.peek()) >= getPriority(t) && !isRightAssoc(stack.peek())) {
+                            getPrecedence(stack.peek()) >= getPrecedence(t) && !isRightAssoc(stack.peek())) {
                         result.add(stack.pop());
                     }
+                    // fallthrough
                 case LEFTPAR:
                     stack.push(t);
                     break;
