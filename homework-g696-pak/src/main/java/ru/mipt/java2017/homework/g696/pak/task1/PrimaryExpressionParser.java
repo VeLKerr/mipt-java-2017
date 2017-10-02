3ac6fbaef8eda0740b6b659c9e1a0854840ae276
paknikolay <pak_kolya98@mail.ru>
2017-10-02 18:00:17 +0300

    Add files via upload

---
+++ homework-g696-pak/src/main/java/ru/mipt/java2017/homework/g696/pak/task1/PrimaryExpressionParser.java
@@ -0,0 +1,35 @@
+package ru.mipt.java2017.homework.g696.pak.task1;
+import sun.security.pkcs.ParsingException;
+
+//parse numbers
+public class PrimaryExpressionParser implements ExpressionParser {
+
+    private int getDigit_(char digit) {
+        return (digit - '0');
+    }
+
+    @Override
+    public Calculable parse(String expression) throws ParsingException {
+        double frictionalPart = 0, integerPart = 0;
+        int iter = 0;
+        int sign = 1;
+        if (expression.charAt(0) == '-') {
+            sign = -1;
+            ++iter;
+        }
+        for (; iter < expression.length() && expression.charAt(iter) != '.'; ++iter) {
+            integerPart = integerPart * 10 + getDigit_(expression.charAt(iter));
+        }
+
+        if (iter < expression.length() && expression.charAt(iter) == '.')
+            ++iter;
+
+        double e = 1;
+        for (; iter < expression.length(); ++iter) {
+            e *= 10;
+            frictionalPart = frictionalPart * 10 + getDigit_(expression.charAt(iter));
+        }
+        return new Constant(sign * (integerPart + frictionalPart / e));
+    }
+
+}
---
+++ homework-g696-pak/src/test/java/ru/mipt/java2017/homework/g696/pak/task1/TestCalc.java
@@ -0,0 +1,38 @@
+package ru.mipt.java2017.homework.g696.pak.task1;
+
+import sun.security.pkcs.ParsingException;
+import org.junit.Test;
+public class TestCalc extends AbstractCalculatorTest {
+
+    @Override
+    protected Calculator calc() {
+        return new Calculator();
+    }
+
+    @Test(expected = ParsingException.class)
+    public void divDivTest() throws ParsingException {
+        tryFail("1//5");
+    }
+
+    @Test(expected = ParsingException.class)
+    public void minusMinusTest() throws ParsingException {
+        tryFail("1--5");
+    }
+
+    @Test(expected = ParsingException.class)
+    public void MultMultTest() throws ParsingException {
+        tryFail("1**5");
+    }
+
+    @Test
+    public void testLargeExepression() throws ParsingException {
+        test("1.1+5 -(-4.2)/5 * (5-4*-1)+(4*1/2) - 44 - ((-5)/5 *6+(6*8-4))",
+            1.1 + 5 - (-4.2) / 5 * (5 - 4 * -1) + (4 * 1 / 2) - 44 - ((-5) / 5 * 6 + (6 * 8 - 4)));
+    }
+
+    @Test
+    public void testInf() throws ParsingException {
+        test("1/0.0 + 4", Double.POSITIVE_INFINITY);
+        test("1/-1/(-5 - (-5))      -(-(-100))", Double.NEGATIVE_INFINITY);
+    }
+}
