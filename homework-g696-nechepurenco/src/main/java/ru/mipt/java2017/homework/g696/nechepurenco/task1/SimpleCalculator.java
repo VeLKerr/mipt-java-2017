072b26fb1ef4874a59df09bb23af87a083e529e2
IvanNechepureno <ivan.nechepurenco@yandex.ru>
2017-10-03 20:37:15 +0300

    fix style

--- homework-g696-nechepurenco/src/main/java/ru/mipt/java2017/homework/g696/nechepurenco/task1/SimpleCalculator.java
+++ homework-g696-nechepurenco/src/main/java/ru/mipt/java2017/homework/g696/nechepurenco/task1/SimpleCalculator.java
@@ -1,12 +1,12 @@
 package ru.mipt.java2017.homework.g696.nechepurenco.task1;
 
-import  ru.mipt.java2017.homework.base.task1.Calculator;
-import  ru.mipt.java2017.homework.base.task1.ParsingException;
+import ru.mipt.java2017.homework.base.task1.Calculator;
+import ru.mipt.java2017.homework.base.task1.ParsingException;
 
-public class SimpleCalculator implements  Calculator{
-    public double calculate(String expression) throws ParsingException {
-        StringParser sp = new StringParser(expression);
-        sp.Parse();
-        return sp.getanswer();
-    }
+public class SimpleCalculator implements Calculator {
+  public double calculate(String expression) throws ParsingException {
+    StringParser sp = new StringParser(expression);
+    sp.Parse();
+    return sp.getanswer();
+  }
 }
