15938538ec849ec3850e6886149d635b34b81b8e
Perchuk Svyatoslav <slava.perchuk@gmail.com>
2017-10-04 23:54:13 +0300

    Calculator is ready.

--- homework-g694-perchuk/src/main/java/ru/mipt/java2017/homework/g694/perchuk/task1/Evaluator.java
+++ homework-g694-perchuk/src/main/java/ru/mipt/java2017/homework/g694/perchuk/task1/Evaluator.java
@@ -9,80 +9,85 @@ import java.util.Stack;
  */
 class Evaluator {
 
-    private Stack<Double> operands = new Stack<>();
-    private Stack<Character> operations = new Stack<>();
+  private Stack<Double> operands = new Stack<>();
+  private Stack<Character> operations = new Stack<>();
 
-    private void popOperation() throws ParsingException {
-        if ((operations.isEmpty()) || (operands.size() < 2))
-            throw new ParsingException("in operating");
-
-        double num2 = operands.pop();
-        double num1 = operands.pop();
-
-        switch (operations.pop()) {
-            case '+':
-                operands.push(num1 + num2);
-                break;
-            case '-':
-                operands.push(num1 - num2);
-                break;
-            case '/':
-                operands.push(num1 / num2);
-                break;
-            case '#':
-            case '*':
-                operands.push(num1 * num2);
-                break;
-            default:
-                throw new ParsingException("in operating");
-        }
+  private void popOperation() throws ParsingException {
+    if ((operations.isEmpty()) || (operands.size() < 2)) {
+      throw new ParsingException("in operating");
     }
 
-    static private short priority(Character ch) throws ParsingException{
-        switch (ch) {
-            case '(':
-                return 3;
-            case '+':
-            case '-':
-                return 2;
-            case '*':
-            case '/':
-                return 1;
-            case '#':
-                return 0;
-            default:
-                throw new ParsingException("invalid symbol");
-        }
-    }
+    double num2 = operands.pop();
+    double num1 = operands.pop();
 
-    void addLeftBrace() {
-        operations.push('(');
+    switch (operations.pop()) {
+      case '+':
+        operands.push(num1 + num2);
+        break;
+      case '-':
+        operands.push(num1 - num2);
+        break;
+      case '/':
+        operands.push(num1 / num2);
+        break;
+      case '#':
+      case '*':
+        operands.push(num1 * num2);
+        break;
+      default:
+        throw new ParsingException("in operating");
     }
+  }
 
-    void addRightBrace() throws ParsingException {
-        while((!operations.empty()) && (operations.peek() != '('))
-            popOperation();
-        if (operations.empty())
-            throw new ParsingException("missing brace");
-        operations.pop();
+  static private short priority(Character ch) throws ParsingException {
+    switch (ch) {
+      case '(':
+        return 3;
+      case '+':
+      case '-':
+        return 2;
+      case '*':
+      case '/':
+        return 1;
+      case '#':
+        return 0;
+      default:
+        throw new ParsingException("invalid symbol");
     }
+  }
 
-    void addDouble(Double num) {
-        operands.add(num);
-    }
+  void addLeftBrace() {
+    operations.push('(');
+  }
 
-    void addOperation(Character op) throws ParsingException {
-        while ((!operations.empty()) && (priority(op) >= priority(operations.peek())))
-            popOperation();
-        operations.push(op);
+  void addRightBrace() throws ParsingException {
+    while ((!operations.empty()) && (operations.peek() != '(')) {
+      popOperation();
     }
+    if (operations.empty()) {
+      throw new ParsingException("missing brace");
+    }
+    operations.pop();
+  }
 
-    Double completeEvaluation() throws ParsingException {
-        while (!operations.empty())
-            popOperation();
-        if (operands.size() != 1) {
-            throw new ParsingException("in evaluating");
-        }
-        return operands.pop();
+  void addDouble(Double num) {
+    operands.add(num);
+  }
+
+  void addOperation(Character op) throws ParsingException {
+    while ((!operations.empty()) && (priority(op) >= priority(operations.peek()))) {
+      popOperation();
     }
+    operations.push(op);
+  }
+
+  Double completeEvaluation() throws ParsingException {
+    while (!operations.empty()) {
+      popOperation();
+    }
+    if (operands.size() != 1) {
+      throw new ParsingException("in evaluating");
+    }
+    return operands.pop();
+  }
 }
