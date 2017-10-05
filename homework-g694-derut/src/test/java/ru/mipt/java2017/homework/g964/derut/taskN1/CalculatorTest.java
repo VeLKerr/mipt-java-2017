fbaa0eb59e9149985efea3b2ad2056dca5b02176
Dan <danilderut@gmail.com>
2017-10-05 11:06:55 +0300

    task1

--- homework-g694-derut/src/test/java/ru/mipt/java2017/homework/g964/derut/taskN1/CalculatorTest.java
+++ homework-g694-derut/src/test/java/ru/mipt/java2017/homework/g964/derut/taskN1/CalculatorTest.java
@@ -1,10 +1,8 @@
 
-package ru.mipt.java2017.homework.g964.derut.task1;
+package ru.mipt.java2017.homework.g964.derut.taskN1;
 
 import ru.mipt.java2017.homework.base.task1.Calculator;
 import ru.mipt.java2017.homework.tests.task1.AbstractCalculatorTest;
-
-import ru.mipt.java2017.homework.g964.derut.taskN1.MyCalculator;
 
 
 public class CalculatorTest extends AbstractCalculatorTest {
