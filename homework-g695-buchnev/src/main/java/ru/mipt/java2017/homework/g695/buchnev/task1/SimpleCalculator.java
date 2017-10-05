fad37d4a101a7349cfecdd56a4a166e9591bd065
ShCoDeRoK <buchnev.valentin@gmail.com>
2017-10-05 17:26:54 +0300

    added first homework

---
+++ homework-g695-buchnev/src/main/java/ru/mipt/java2017/homework/g695/buchnev/task1/SimpleCalculator.java
@@ -0,0 +1,4 @@
+package ru.mipt.java2017.homework.g695.buchnev.task1;
+
+public class SimpleCalculator {
+}
---
+++ homework-g695-buchnev/src/test/java/ru/mipt/java2017/homework/g695/buchnev/task1/java2017CalculatorTest.java
@@ -0,0 +1,4 @@
+package ru.mipt.java2017.homework.g695.buchnev.task1;
+
+public class java2017CalculatorTest {
+}
