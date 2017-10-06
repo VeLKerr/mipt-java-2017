a17a66a374489f004f0d870bdccca867b9266a20
alex-kozinov <alex-kozinov@mail.ru>
2017-10-06 23:38:37 +0300

    got code revew

---
+++ homework-g694-kozinov/src/main/java/ru/mipt/java2017/homework/g694/kozinov/task1/CharRecognizer.java
@@ -0,0 +1,74 @@
+package ru.mipt.java2017.homework.g694.kozinov.task1;
+
+class CharRecognizer {
+  public enum CharKind {
+    IGNORE,
+    UNKNOWN,
+    OPEN_BRACKET,
+    CLOSE_BRACKET,
+    NUMBER_PART,
+    OPERATION
+  }
+
+  public enum OperationKind {
+    UNKNOWN,
+    PLUS,
+    MINUS,
+    MULTI,
+    DIVISION;
+    public int getPriority() {
+      if (this == PLUS || this == MINUS) {
+        return 1;
+      }
+
+      if (this == MULTI || this == DIVISION) {
+        return 2;
+      }
+
+      return -1;
+    }
+  }
+
+  static CharKind getCharKind(char c) {
+    if (c == ' ') {
+      return CharKind.IGNORE;
+    }
+
+    if (c == '(') {
+      return CharKind.OPEN_BRACKET;
+    }
+
+    if (c == ')') {
+      return CharKind.CLOSE_BRACKET;
+    }
+
+    if (Character.isDigit(c) || c == '.') {
+      return CharKind.NUMBER_PART;
+    }
+
+    if (c == '+' || c == '-' || c == '*' || c == '/') {
+      return CharKind.OPERATION;
+    }
+
+    return CharKind.UNKNOWN;
+  }
+
+  static OperationKind getOperationKind(char c) {
+    switch (c) {
+      case '+':
+        return OperationKind.PLUS;
+
+      case '-':
+        return OperationKind.MINUS;
+
+      case '*':
+        return OperationKind.MULTI;
+
+      case '/':
+        return OperationKind.DIVISION;
+
+      default:
+        return OperationKind.UNKNOWN;
+    }
+  }
+}
---
+++ homework-g694-kozinov/src/main/java/ru/mipt/java2017/homework/g694/kozinov/task1/Parser.java
@@ -0,0 +1,77 @@
+package ru.mipt.java2017.homework.g694.kozinov.task1;
+
+import ru.mipt.java2017.homework.base.task1.ParsingException;
+import java.util.Vector;
+
+class Parser {
+  private Vector<String> lexemes;
+
+  Parser() {
+    lexemes = new Vector<>();
+  }
+
+  void parseString(String expression) throws ParsingException {
+    int len = expression.length();
+    int balance = 0;
+    lexemes.clear();
+
+    if (expression.isEmpty()) {
+      throw new ParsingException("You write an empty string");
+    }
+
+    for (int i = 0; i < len; ++i) {
+      char currentSymbol = expression.charAt(i);
+      switch (CharRecognizer.getCharKind(currentSymbol)) {
+        case IGNORE:
+          break;
+        case UNKNOWN:
+          throw new ParsingException("Unknown symbol is " + currentSymbol);
+
+        case NUMBER_PART:
+          StringBuilder nextString = new StringBuilder();
+          while (CharRecognizer.getCharKind(currentSymbol) ==
+            CharRecognizer.CharKind.NUMBER_PART) {
+            nextString.append(currentSymbol);
+            ++i;
+            if (i >= len) {
+              break;
+            }
+
+            currentSymbol = expression.charAt(i);
+          }
+          --i;
+          lexemes.add(nextString.toString());
+          break;
+
+        case OPEN_BRACKET:
+          ++balance;
+          lexemes.add("(");
+          break;
+
+        case CLOSE_BRACKET:
+          --balance;
+          if (balance < 0) {
+            throw new ParsingException("Extra close bracket at position " + i);
+          }
+
+          lexemes.add(")");
+          break;
+
+        default:
+          lexemes.add("" + currentSymbol);
+      }
+    }
+
+    if (balance != 0) {
+      throw new ParsingException("Number of close and open brackets are different");
+    }
+  }
+
+  String getAt(int i) {
+    return lexemes.get(i);
+  }
+
+  int size() {
+    return lexemes.size();
+  }
+}
