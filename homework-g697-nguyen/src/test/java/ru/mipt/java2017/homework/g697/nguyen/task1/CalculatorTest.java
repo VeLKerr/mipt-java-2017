c37c013ee9ed101f54ecc4811ad21035c9443b1c
nguyenhanh52889 <nguyenhanh.52889@gmail.com>
2017-10-06 19:58:40 +0700

    Add files via upload

--- homework-g697-nguyen/src/test/java/ru/mipt/java2017/homework/g697/nguyen/task1/CalculatorTest.java
+++ homework-g697-nguyen/src/test/java/ru/mipt/java2017/homework/g697/nguyen/task1/CalculatorTest.java
@@ -4,8 +4,9 @@ import ru.mipt.java2017.homework.base.task1.Calculator;
 import ru.mipt.java2017.homework.tests.task1.AbstractCalculatorTest;
 
 public class CalculatorTest extends AbstractCalculatorTest {
-    @Override
-    protected Calculator calc() {
-        return new MyCalculator();
-    }
+
+  @Override
+  protected Calculator calc() {
+    return new MyCalculator();
+  }
 }
