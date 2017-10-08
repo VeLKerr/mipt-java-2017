404a8d1bd894acc22c983346920f9be1fab10c21
Polyansky Maxim <polyanskimax@gmail.com>
2017-10-08 17:28:18 +0300

    Rejected commit: Default

--- homework-g695-polyanskiy/src/test/java/ru/mipt/java2017/homework/g695/polyanskiy/task1/MyCalculatorTest.java
+++ homework-g695-polyanskiy/src/test/java/ru/mipt/java2017/homework/g695/polyanskiy/task1/MyCalculatorTest.java
@@ -1,7 +1,23 @@
 package ru.mipt.java2017.homework.g695.polyanskiy.task1;
 
-/**
- * Created by maxim on 06.10.17.
- */
-public class MyCalculatorTest {
+
+import org.junit.Test;
+import ru.mipt.java2017.homework.base.task1.ParsingException;
+import ru.mipt.java2017.homework.base.task1.Calculator;
+import ru.mipt.java2017.homework.tests.task1.AbstractCalculatorTest;
+
+public class MyCalculatorTest extends AbstractCalculatorTest {
+
+  @Override
+  protected Calculator calc() {
+    return new MyCalculator();
+  }
+
+  @Test
+  public void SomeTest() throws ParsingException {
+    test("0+0.0+0", 0);
+    test("+(-(+(-(0)))) - 1", -1);
+  }
+
 }
+
